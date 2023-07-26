d2ae4ed913eba0578041090c03b3df6e52139bdd
AnitaRak <anitatantely@gmail.com>
2023-07-26 11:39:56 +0300

    update traimtement login

--- Application/app/src/main/java/com/example/application/LoginActivity.java
+++ Application/app/src/main/java/com/example/application/LoginActivity.java
@@ -11,10 +11,11 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.application.model.Profil;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText loginText , passwordText ;
+    TextInputEditText loginText , passwordText ;
     Button btnLogin ;
 
     private static final String TAG = "LoginActivity";
@@ -24,12 +25,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        loginText = (EditText) findViewById(R.id.user);
-        passwordText = (EditText) findViewById((R.id.password));
-        String username = loginText.getText().toString();
-        String password = passwordText.getText().toString();
-        Log.i(TAG, "laaaaaaaaaaaaaaaaaaaaa"+ username);
-        Log.i(TAG, "looooooooooooooooooooo"+password);
+        loginText = (TextInputEditText) findViewById(R.id.user);
+        passwordText = (TextInputEditText) findViewById(R.id.password);
+
         btnLogin = (Button) findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +35,8 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 /*String username = loginText.getText().toString();
                 String password = passwordText.getText().toString();*/
-                Log.i(TAG, username);
-                Log.i(TAG, password);
+                String username = loginText.getText().toString();
+                String password = passwordText.getText().toString();
 
                 Profil profil = new Profil();
                 profil.traitementLogin(username, password, new Profil.LoginCallback() {
