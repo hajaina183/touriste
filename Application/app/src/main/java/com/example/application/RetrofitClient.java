19a2e77203410df7a376c061d6f8b9300253000a
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-08-07 00:46:13 +0400

    retofit client - base url web service

--- Application/app/src/main/java/com/example/application/RetrofitClient.java
+++ Application/app/src/main/java/com/example/application/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     private static Retrofit retrofit;
-    private static String BASE_URL = "http://192.168.172.68:3000/";
+    private static String BASE_URL = "https://touriste-madagascar-webservice.onrender.com/";
 
     public static Retrofit getRetrofitInstance(){
         if(retrofit == null){
